9788d2fe9dedd4987ccd107953b481372cb6f4b3
Armando Fox <fox@cs.berkeley.edu>
2014-06-10 16:42:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moocchat::Application.config.secret_token = 'de165726bbb407c864891a44868e8a155667213d5aaaacc3309afc0b06c60de8737a433aa31b333f12378c13b34d9f3246747b4cd67eed27b67320876d102ddc'
