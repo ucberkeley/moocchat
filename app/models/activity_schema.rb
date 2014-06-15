6f744ba1ca64c9bee05ee24edd6a3cc5b7be9057
Derrick Coetzee <dc@moonflare.com>
2014-06-15 15:20:18 -0700

    Add initial version of seed data for Condition, Cohort, ActivitySchema; add migration to add questions field to ActivitySchema; set up ActivitySchema model with has_many_inline for questions; set up attr_accessible for several models to allow seeding

--- app/models/activity_schema.rb
+++ app/models/activity_schema.rb
@@ -1,9 +1,11 @@
 class ActivitySchema < ActiveRecord::Base
+  include HasManyInline
   has_many :tasks
   belongs_to :cohort
-  serialize :questions, Array
 
   validates_presence_of :name
-  attr_accessible :name, :enabled
+  attr_accessible :name, :enabled, :cohort, :questions, :randomized, :num_questions, :tag
+
+  has_many_inline :questions, :class_name => :question
 
 end
--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,7 +2,7 @@ class Condition < ActiveRecord::Base
   include HasManyInline
   has_many :tasks
 
-  attr_accessible :name
+  attr_accessible :name, :prologue_pages, :body_pages, :epilogue_pages
 
   has_many_inline :prologue_pages, :class_name => :template
   has_many_inline :body_pages, :class_name => :template
