942a87ca6dc399cc7458f98bbfc361b95d65bb8f
Robert Hernandez <r.hernandez@berkeley.edu>
2014-06-16 17:17:13 -0700

    sad path done, condition rspec done, but static task page rspec incomplete

--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -1,7 +1,10 @@
+require "spec_helper"
+
 describe TasksController do
   describe "routing" do
-    it "routes to #static" do
-      get("/").should route_to("tasks#static")
+  	
+    it "routes to tasks#static" do
+    	expect(get("/")).to route_to("tasks#static")
     end
   end
 end
