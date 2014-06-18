3580d709d85301e2a7b093a8b5258f30f4d2668f
Robert Hernandez <r.hernandez@berkeley.edu>
2014-06-18 12:57:36 -0700

    Bootstrap Installed Bootstrap installed added bootstrap to scaffold and updated gemfile with dependencys for bootstrap
    modifying css on static page
    
    added wrapper to static page
    
    adding color
    
    added a bit of style to welcome page
    
    checkpoint for static page
    
    working on adding explaination
    
    revert to old edit

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   serialize :answers, Array
-  attr_accessible  :answers, :text,:correct_answer_index
+  attr_accessible  :answers, :text,:correct_answer_index, :explanation
 
 end
