bae4dc8b67f0319c02ccae064c05ece20c62433f
Robert Hernandez <r.hernandez@berkeley.edu>
2014-06-18 02:25:32 -0700

    corrected format for step_defition and corrected format of feature

--- features/step_definitions/static_steps.rb
+++ features/step_definitions/static_steps.rb
@@ -3,9 +3,8 @@ Given(/^I start on the Static Page$/) do
 end
 
 Then(/^I should see a JS dialog saying "(.*?)"$/) do |statement|
-  #page.driver.browser.switch_to.alert.accept 
    var = page.driver.browser.switch_to.alert.text
-   var == "please fill out all the form"
+   var == statement 
    page.driver.browser.switch_to.alert.accept
 end
 
