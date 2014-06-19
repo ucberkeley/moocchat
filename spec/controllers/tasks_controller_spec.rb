06d95d71697927da6c4acf9ba664e2013e76e82f
Armando Fox <fox@cs.berkeley.edu>
2014-06-19 13:46:34 -0700

    moved task routing specs to spec/routing/

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,27 +2,6 @@
 require 'spec_helper'
 
 describe TasksController do
-  describe 'task URI', :type => :routing do
-    it 'creates new task on POST' do
-      expect(:post => '/tasks/armandofox/237/15').to route_to(
-        :controller => 'tasks',
-        :action => 'create',
-        :learner_name => 'armandofox',
-        :activity_schema_id => '237',
-        :condition_id => '15'
-        )
-    end
-    it 'does not create task on GET' do
-      expect(:get => '/tasks/armandofox/237/15').not_to be_routable
-    end
-    it 'routes to Welcome page' do
-      expect(:get => '/tasks/10').to route_to :controller => 'tasks', :action => 'welcome', :id => '10'
-    end
-    it 'routes to page render' do
-      expect(:get => '/tasks/10/page').to route_to :controller => 'tasks', :action => 'page', :id => '10'
-    end
-  end
-
   describe 'establishing session' do
     before :all do 
       @dummy_params = { :activity_schema_id => '0', :learner_name => 'x', :condition_id => '1' }
