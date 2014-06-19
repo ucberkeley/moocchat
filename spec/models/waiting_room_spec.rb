6e5e9e1b348a52a584c8570ee2e76726b7bbc6cf
Armando Fox <fox@cs.berkeley.edu>
2014-06-19 11:45:49 -0700

    Basic specs for adding a task to waiting room and making sure no duplicate tasks in waiting room

--- spec/models/waiting_room_spec.rb
+++ spec/models/waiting_room_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 describe WaitingRoom do
   describe 'adding task' do
-    context 'when waiting room exists' do
-      before :each do ; @w = create :waiting_room ; end
-      it 'adds task to waiting room' do
-        
-      end
+    before :each do ; @t = create :task ; end
+    it 'creates waiting room when doesn\'t exist for this activity/condition' do
+      expect { WaitingRoom.add(@t) }.to change { WaitingRoom.count }.by 1
+    end
+    it 'does not create waiting room if it exists' do
+      create :waiting_room, :activity_schema => @t.activity_schema, :condition => @t.condition
+      expect { WaitingRoom.add(@t) }.not_to change { WaitingRoom.count }
+    end
+    it 'raises exception if same task added >1 time' do
+      WaitingRoom.add @t
+      expect { WaitingRoom.add @t }.to raise_error(WaitingRoom::TaskAlreadyWaitingError)
     end
   end
 
