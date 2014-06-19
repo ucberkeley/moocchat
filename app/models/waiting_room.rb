9ee8b73fe087963d41b394a4092513853b81871f
Armando Fox <fox@cs.berkeley.edu>
2014-06-19 11:45:49 -0700

    Basic specs for adding a task to waiting room and making sure no duplicate tasks in waiting room

--- app/models/waiting_room.rb
+++ app/models/waiting_room.rb
@@ -18,6 +18,10 @@ class WaitingRoom < ActiveRecord::Base
   #
   has_many :tasks
 
+  # It is an error to try to enqueue (put in a waiting room) the same
+  # task more than once. 
+  class ::TaskAlreadyWaitingError < RuntimeError ; end
+
   # When the class method +process_all!+ is called, all waiting rooms
   # are checked to see if any of them have an expired timer, and
   # +WaitingRoom#process!+ is called on any ready instances.  The call
@@ -25,5 +29,20 @@ class WaitingRoom < ActiveRecord::Base
   # the method to an authenticated endpoint.
   #
 
+  public
+
+  # Add a task to a waiting room.
+  # If the waiting room for this condition and activity doesn't exist,
+  # create it.
+  def self.add task
+    wr = WaitingRoom.
+      find_or_create_by_activity_schema_id_and_condition_id!(
+      task.activity_schema_id, task.condition_id)
+    if wr.tasks.include? task
+      raise TaskAlreadyWaitingError
+    else
+      wr.tasks << task
+    end
+  end
 
 end
