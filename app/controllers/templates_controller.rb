fb022772459cd112d53a772e3bf2cb1cd1d1cf60
Robert Hernandez <r.hernandez@berkeley.edu>
2014-06-20 16:51:03 -0700

    ready for deploy Rebasing commit
    allow derrick's seed to work on my computer
    
    just css and no less bootstrap
    
    css update
    
    illegal remove of id
    
    css updated
    
    fixed bug in view
    
    params question added

---
+++ app/controllers/templates_controller.rb
@@ -0,0 +1,83 @@
+class TemplatesController < ApplicationController
+	# GET /templates
+  # GET /templates.json
+  def index
+    @templates = Template.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @templates }
+    end
+  end
+
+  # GET /templates/1
+  # GET /templates/1.json
+  def show
+    @template = Template.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @template }
+    end
+  end
+
+  # GET /templates/new
+  # GET /templates/new.json
+  def new
+    @template = Template.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @template }
+    end
+  end
+
+  # GET /templates/1/edit
+  def edit
+    @template = Template.find(params[:id])
+  end
+
+  # POST /templates
+  # POST /templates.json
+  def create
+    @template = Template.new(params[:template])
+
+    respond_to do |format|
+      if @template.save
+        format.html { redirect_to @template, notice: 'Template was successfully created.' }
+        format.json { render json: @template, status: :created, location: @template }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @template.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /templates/1
+  # PUT /templates/1.json
+  def update
+    @template = Template.find(params[:id])
+
+    respond_to do |format|
+      if @template.update_attributes(params[:template])
+        format.html { redirect_to @template, notice: 'Template was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @template.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /templates/1
+  # DELETE /templates/1.json
+  def destroy
+    @template = Template.find(params[:id])
+    @template.destroy
+
+    respond_to do |format|
+      format.html { redirect_to templates_url }
+      format.json { head :no_content }
+    end
+  end
+end
