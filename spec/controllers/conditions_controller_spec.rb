524bbc53f858954f5181b2468bc6bd8e4eae5f08
Armando Fox <fox@cs.berkeley.edu>
2014-06-20 14:10:27 -0700

    Added conditions spec and changes to conditions controller spec to allow passing with the validations on the group size attributes

--- spec/controllers/conditions_controller_spec.rb
+++ spec/controllers/conditions_controller_spec.rb
@@ -23,7 +23,7 @@ describe ConditionsController do
   # This should return the minimal set of attributes required to create a valid
   # Condition. As you add validations to Condition, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { {  } }
+  let(:valid_attributes) { { :name => 'dummy', :minimum_group_size => 1, :preferred_group_size => 2 } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
