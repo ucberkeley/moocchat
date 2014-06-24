dc8817885ed28c0cc207a74e9def3e631078f0bb
Armando Fox <fox@cs.berkeley.edu>
2014-06-24 00:21:48 -0700

    Working version of timer, but still cannot get jasmine to run specs

---
+++ spec/javascripts/timer_spec.js
@@ -0,0 +1,8 @@
+//= require jquery-2.0.3.min
+//= require helpers/jasmine-jquery
+//= require timer
+describe('Timer', function() {
+   it('works', function() {
+     expect(true).toBe(false);
+   }
+});
