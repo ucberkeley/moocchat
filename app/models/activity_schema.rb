2219609949d9f13e6550aff68eb0c6bff47a674c
Robert Hernandez <r.hernandez@berkeley.edu>
2014-07-08 16:02:54 -0700

    recover has many eventlog

--- app/models/activity_schema.rb
+++ app/models/activity_schema.rb
@@ -1,6 +1,8 @@
 class ActivitySchema < ActiveRecord::Base
   include HasManyInline
   has_many :tasks
+  has_many :event_logs
+
   belongs_to :cohort
 
   # Minimum interval between experiment starts, in minutes
