5e3130400336c5b3401b687b070873416d8d2653
Armando Fox <fox@cs.berkeley.edu>
2014-08-04 13:01:25 -0700

    ContinueButton handler should not fire when Send Chat button is pressed

--- app/assets/javascripts/continue_button.js
+++ app/assets/javascripts/continue_button.js
@@ -29,7 +29,7 @@ var ContinueButton =  {
       return;
     }
     $('#interstitial').hide();
-    $('body').on('click', ':submit', ContinueButton.sendForm);
+    $('body').on('click', ':submit:not(#send)', ContinueButton.sendForm);
   }
 };
 
