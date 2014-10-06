d0482853ae6fe580fc3a19171328782f50c90389
Armando Fox <fox@cs.berkeley.edu>
2014-10-05 19:03:54 -0700

    wrapper around httperf for testing Task#create, which is heaviest-weight call

--- benchmark/create_task.sh
+++ benchmark/create_task.sh
@@ -1,2 +1,8 @@
-#!/bash
-httperf --server=moocchat-load.herokuapp.com --method=POST --uri=/tasks/Learner+1/1/1 --num-conns=100 --rate=20 --timeout=5
+#!/bin/bash
+NUM=$1
+: ${NUM:=1}
+RATE=$2
+: ${RATE:=10}
+TIMEOUT=$3
+: ${TIMEOUT:=5}
+httperf --server=moocchat-load.herokuapp.com --method=POST --uri=/tasks/Learner+1/1/1 --num-conns=$NUM --rate=$RATE --timeout=$TIMEOUT
