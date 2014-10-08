e79f0dd7db18139fc75771b3aef9800fe4eaaee1
liyeungjohn <liyeungjohn@berkeley.edu>
2014-10-08 16:12:55 -0700

    websockets verify receiving message sent from 1 websocket

--- loadTesting/ws.py
+++ loadTesting/ws.py
@@ -1,14 +1,21 @@
 from websocket import create_connection
 import json
 import sys
+
+def construct_response(learner_number, input):
+	message = json.loads(input)
+	message["text"] = "Learner " + str(learner_number) + ": " + 	message["text"]
+	return message
+
 #https://pypi.python.org/pypi/websocket-client/
 websockets = []
-number = 20
-groupSize = 10
+number = 500
+groupSize = 500
 first_taskid = 0
 
 while number > 0:
 	websockets = []
+	message_sent = {}
 	size = 0
 	if number % groupSize == 0:
 		size = groupSize
@@ -21,31 +28,31 @@ while number > 0:
 	for taskid in range(first_taskid + 1, last_taskid):
 		group = group + "," + str(taskid)
 	print "group: " + group
-	#First web socket
-	data1 = {"text": "testLoading", "taskid": first_taskid, "type": "message"}
-	url1 = "wss://moocchat-john.herokuapp.com/" + group + "," + str(first_taskid)
-	ws1 = create_connection(url1)
-	websockets.append(ws1)
-	ws1.send(json.dumps(data1))
 
 	#create a lot of web sockets
 	for taskid in range(first_taskid, last_taskid):
 		url = "wss://moocchat-john.herokuapp.com/" + group + "," + str(taskid)
 		ws = create_connection(url)
 		websockets.append(ws)
+
 	#send data from ws1
-	#ws1.send(json.dumps(data1))
+	data = json.dumps({"text": "testLoading", "taskid": first_taskid, "type": "message"})
+	websockets[0].send(data)
+	message_sent[str(first_taskid)] = construct_response(1, data)
+
+	#receive and close number of web sockets
+	for ws in websockets:
+		result = json.loads(ws.recv())
+		expected = message_sent[str(first_taskid)]
+		if expected != result:
+			print "Failed: different meessage! Expected: " + expected + "| Got: " + result
+			sys.exit(1)
+		ws.close()
 	first_taskid = last_taskid
 	number = number - size
-	for ws in websockets:
-		#result = ws.recv()
-		#print "Received '%s'" % result
-		ws.close()
-#receive and close number of web sockets
-#for ws in websockets:
-	#result = ws.recv()
-	#print "Received '%s'" % result
-	#ws.close()
+	print "Finish: " + str(size) + " websockets"
 
-print "Done, finish: " + str(number) + " websockets"
 sys.exit(0)
+
+
+
