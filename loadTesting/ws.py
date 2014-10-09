889ed58e101efc0ce8e7038fb15c61cb7a945f3e
liyeungjohn <liyeungjohn@berkeley.edu>
2014-10-08 17:25:05 -0700

    stat prints  & store to file

--- loadTesting/ws.py
+++ loadTesting/ws.py
@@ -1,22 +1,37 @@
 from websocket import create_connection
 import json
 import sys
+import timeit
+#https://pypi.python.org/pypi/websocket-client/
+#best = 538
 
 def construct_response(learner_number, input):
 	message = json.loads(input)
 	message["text"] = "Learner " + str(learner_number) + ": " + 	message["text"]
 	return message
 
-#https://pypi.python.org/pypi/websocket-client/
-websockets = []
-number = 500
+def print_store_stat():
+	print "Finish a total of: " + str(total_completed) + " websockets"
+	print "Took a total of: " + str(sum(runtimes))
+	statFile.write("Finish a total of: " + str(total_completed) + " websockets" + "\n")
+	statFile.write("Took a total of: " + str(sum(runtimes)) + "\n")
+
+def print_store(record):
+	print str(record)
+	statFile.write(record + "\n")
+
+number = 1000
 groupSize = 500
 first_taskid = 0
 
+total_completed = 0
+runtimes = []
+statFile = open("testLoading.txt", "w")
 while number > 0:
 	websockets = []
 	message_sent = {}
 	size = 0
+	start = timeit.default_timer()
 	if number % groupSize == 0:
 		size = groupSize
 	else:
@@ -27,7 +42,6 @@ while number > 0:
 	group = str(first_taskid)
 	for taskid in range(first_taskid + 1, last_taskid):
 		group = group + "," + str(taskid)
-	print "group: " + group
 
 	#create a lot of web sockets
 	for taskid in range(first_taskid, last_taskid):
@@ -45,13 +59,24 @@ while number > 0:
 		result = json.loads(ws.recv())
 		expected = message_sent[str(first_taskid)]
 		if expected != result:
-			print "Failed: different meessage! Expected: " + expected + "| Got: " + result
+			print_store("Failed: different meessage! Expected: " + expected + "| Got: " + result)
 			sys.exit(1)
 		ws.close()
+		total_completed = total_completed + 1	
+
+	#wrap up for next loop
 	first_taskid = last_taskid
 	number = number - size
-	print "Finish: " + str(size) + " websockets"
+	stop = timeit.default_timer()
+	runtime = stop - start
+	runtimes.append(runtime)
+	print_store("group of taskid: " + str(group) + "\n")
+	print_store("map of message_sent: " + str(message_sent) + "\n")
+	print_store("Finish: " + str(size) + " websockets")
+	print_store("Took: " + str(runtime) + "\n\n\n")
 
+
+print_store_stat()
 sys.exit(0)
 
 
