b02c878463e0fe8a2b18234a53e9f56a82c1dc7a
Armando Fox <fox@cs.berkeley.edu>
2014-10-10 12:03:51 -0700

    benchmarking with real param sweep

--- benchmark/rails/benchmark.pl
+++ benchmark/rails/benchmark.pl
@@ -7,7 +7,7 @@ $gHttperf = 'httperf';
 
 sub run_test {
     my($num_tasks, $rate, $timeout) = @_;
-    $cmd = "$gHttperf --server=$gServer --method=POST --uri=/tasks/Learner+1/1/1 --num-conns=$num_tasks --rate=$rate --timeout=$timeout";
+    $cmd = "$gHttperf --server=$gServer --method=POST --uri=/tasks/Learner+1/1/1 --num-conns=$num_tasks --rate=$rate --timeout=$timeout 2>/dev/null";
     warn "Running '$cmd'\n";
     $output = `$cmd`;
     @results = &parse_results($output);
@@ -46,16 +46,19 @@ sub parse_results {
 }
 
 sub main {
-    @rates = (10, 11, 12, 13, 14, 15, 16);
-    @runs = 4;
+    $min_rate = 10;
+    $max_rate = 16;
+    $rate_step = 1;
+    $runs = 4;
+    $tasks_to_create = 1000;
     $clear = "NEWRELIC_AGENT_ENABLED=false heroku run --app $gAppname rails runner -e production Task.delete_all >/dev/null 2>&1";
     $timeout = 10;
     open(OUTPUT, "> output.txt") or die;
     $out = OUTPUT;
     printf $out "| run | rate| min | max | med |stdv |fail%| dur |\n";
-    foreach $rate (@rates) {
-        foreach $run (@runs) {
-            @result = &run_test(20, $rate, $timeout);
+    for ($rate = $min_rate; $rate <= $max_rate; $rate += $rate_step) {
+        for ($run = 1; $run <= $runs; $run++) {
+            @result = &run_test($tasks_to_create, $rate, $timeout);
             printf $out ("| %3d | %3d | %2.1f | %2.1f | %2.1f | %2.1f | %2d  |%4.0f |\n", $run, $rate, @result);
         }
         warn "running '$clear'\n";
