b3cd76a016c2dd0b2ec8b7cb3dddaca52440c282
liyeungjohn <liyeungjohn@berkeley.edu>
2014-10-19 21:22:00 -0700

    fix db migration problem

--- db/migrate/20141014154022_change_start_page_time_to_datetime.rb
+++ db/migrate/20141014154022_change_start_page_time_to_datetime.rb
@@ -1,9 +1,11 @@
 class ChangeStartPageTimeToDatetime < ActiveRecord::Migration
   def up
-    change_column :tasks, :start_page_time, :datetime
+    remove_column :tasks, :start_page_time
+    add_column :tasks, :start_page_time, :datetime
   end
 
   def down
-    change_column :tasks, :start_page_time, :time
+    remove_column :tasks, :start_page_time
+    add_column :tasks, :start_page_time, :time
   end
 end
