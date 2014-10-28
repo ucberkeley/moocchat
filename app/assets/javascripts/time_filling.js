d5d8c676a7fb242bc81f52eecba88dc36c80fed2
clairethompson <cthompson44@berkeley.edu>
2014-10-27 23:57:32 -0700

    framework for js helper for time filler

---
+++ app/assets/javascripts/time_filling.js
@@ -0,0 +1,13 @@
+var time_filling = {
+  setup: function() {
+    var answerBox = $('.moocchat-choice');
+
+    answerBox.click(function() {
+      // IF ANSWER IS WRONG: HIGHLIGHT CORRECT ANSWER
+    });
+    $('#next-q').click(function() {
+      // SHOW NEXT QUESTION
+    });
+  }
+};
+$(time_filling.setup);
