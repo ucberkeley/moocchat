c8d42c3d3dbf5c60634461fb706da3903ee2c366
D Coetzee <dc@moonflare.com>
2015-01-07 14:03:17 -0800

    First iteration of chat log report, shows a separate event log table for each chat group, not pretty but usable at this stage

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,10 +2,13 @@ class ReportsController < ApplicationController
   def chatlog
   end
 
+  def parse_datetime_picker_date(d)
+    return DateTime.strptime(d, '%m/%d/%Y %l:%M %p')
+  end
+
   def chatlog_show
-    puts params
-    @eventlog = EventLog.first
-    puts @eventlog.methods
+    @eventlog = EventLog.where(created_at: parse_datetime_picker_date(params[:datetime_start])..parse_datetime_picker_date(params[:datetime_end]))
+    puts @eventlog
 
     respond_to do |format|
       format.html # 'chatlog_show.html.erb'
